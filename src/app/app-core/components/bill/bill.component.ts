735ee8ec3945f80e654fea4f868cb7d3ef494b3a
BorisKarlof <boriskarlof122@gmail.com>
2021-01-28 13:23:53 +0200

    finished tables for bill component, added tables refresh function

--- src/app/app-core/components/bill/bill.component.ts
+++ src/app/app-core/components/bill/bill.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ValutesService} from '../../services/valutes.service';
 import {Bill} from '../../../shared/interface/bill';
 import {Rates} from '../../../shared/interface/rates';
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-bill',
@@ -12,25 +15,49 @@ export class BillComponent implements OnInit {
   public valutes: Rates[] = [];
   public bill: Bill[] = [];
   public resDate!: string;
-  public displayedColumns = ['EUR', 'USD', 'UAH'];
   private valutesNames = ['EUR', 'USD', 'UAH'];
-
+  private cols = ['valutes', 'values', 'date'];
 
   constructor(
-    private valute: ValutesService
+    private valute: ValutesService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
   ) { }
 
   ngOnInit(): void {
+    this.getValues();
+    this.registerIcons();
+  }
+
+  public getValues(): void {
     this.valute.requestValutes()
       .subscribe((res) => {
-        this.bill.push(res[0]);
         this.resDate = res[1].date;
         this.valutes = Array(3)
           .fill(0)
           .map((i: Rates, x: number) => ({name: this.valutesNames[x], value: res[1].rates[this.valutesNames[x]]}));
-        console.log(this.valutes, this.bill);
-      });
-
+        this.bill = Array(3)
+          .fill(0)
+          .map((i: Bill, x: number) => (
+            { value: res[0].value * this.valutes[x].value, currency: this.valutesNames[x]}
+          ));
+    });
   }
 
+  public registerIcons(): void {
+    this.matIconRegistry.addSvgIcon(
+      `usdd`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../assets/img/usd.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      `uah`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../assets/img/uah.svg')
+    );
+  }
+
+  public refresh(): void{
+    this.valutes = [];
+    this.bill = [];
+    this.getValues();
+  }
 }
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -4,7 +4,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -23,7 +23,7 @@ import {MatTableModule} from '@angular/material/table';
     MatToolbarModule,
     MatSidenavModule,
     MatMenuModule,
-    MatTableModule
+    MatTableModule,
   ],
   exports: [
     MatDialogModule,
